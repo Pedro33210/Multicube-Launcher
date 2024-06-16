34280e585d83406ed0ee2ca404a76aa8337fd1ac
Pedro <daconceicao.pedro101@gmail.com>
2024-06-16 10:07:25 +0200

    Lanch update phrase

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Chargement des blocs...", "author": "Multicube" },
+            { "message": "Attention aux explosions inattendues !", "author": "Multicube" },
+            { "message": "Loading chunks...", "author": "Multicube" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
